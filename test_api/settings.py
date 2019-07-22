77d3a3e6ba398bb431eb1c1a08a06ce8a7d05e57
amartinez <amartinez@ecosoft.com.mx>
2019-07-22 15:24:39 -0500

    se agrega CorsMi.. antes de CommonsMi...

--- test_api/settings.py
+++ test_api/settings.py
@@ -42,11 +42,10 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
-MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+MIDDLEWARE = [    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
